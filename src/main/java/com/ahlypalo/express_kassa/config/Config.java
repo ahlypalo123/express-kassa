794531074003338ce64a0fa29040b20601e1e60e
ahlypalo <ahlypalo@inxeria.com>
2022-06-05 11:45:16 +0300

    Revert "added receipt template repo"
    This reverts commit f386b327dd98bd64cc8fe17d27eeb4af3d443ca0.

--- src/main/java/com/ahlypalo/express_kassa/config/Config.java
+++ src/main/java/com/ahlypalo/express_kassa/config/Config.java
@@ -2,7 +2,6 @@ package com.ahlypalo.express_kassa.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,11 +20,6 @@ public class Config {
   @Bean
   public SecurityContextHolderAwareRequestFilter securityContextHolderAwareRequestFilter() {
     return new SecurityContextHolderAwareRequestFilter();
-  }
-
-  @Bean
-  public ObjectMapper objectMapper() {
-    return new ObjectMapper();
   }
 
   @Bean
